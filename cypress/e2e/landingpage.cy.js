1851ce4bb8b002a39fe8fb66ca58e682ac88e5a0
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2023-10-20 08:19:58 -0400

    Add passing cypress test

--- cypress/e2e/landingpage.cy.js
+++ cypress/e2e/landingpage.cy.js
@@ -10,9 +10,24 @@ describe("landing page", () => {
     );
   });
 
-  it("should display a header and four movie cards", () => {
-    cy.get(".main-header")
-      .contains("Rancid Tomatillos")
-      .get(".poster-container");
-  });
-});
+  it('should display a header and four movie cards', () => {
+    cy.get('.main-header').contains('Rancid Tomatillos')
+    .get('.movie-container').find('.poster-container').should('have.length', 4)
+    .get('.movie-title').first().contains('p', 'Black Adam')
+    .get('.movie-rating').first().contains('p', '4')
+    .get('.movie-title').last().contains('p', 'Black Panther: Wakanda Forever')
+    .get('.movie-rating').last().contains('p', '4')
+  })
+})
+
+it('should display error if user types incorrect URL path', () => {
+  // cy.visit('http://localhost:3000/unexpectedpath')
+
+  // get a p tag that displays an error message 
+  // get a button to return to the home page. use .click()
+  // after clicking home button, the URL should equal the base domain http://localhost:3000/
+
+  // can we pass setError from one component to another (app to movieBlowUp?)
+  // use useState to set error state in movieBlowUp?
+})
+
