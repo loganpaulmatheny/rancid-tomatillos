07a1402e2cc5c049ccb64780b38b82d4b3bdbdae
Logan Matheny <loganpaulmatheny@gmail.com>
2023-10-20 21:03:36 -0400

    Fix errors and complete search functionality

--- src/Components/AllMovies/AllMovies.js
+++ src/Components/AllMovies/AllMovies.js
@@ -1,45 +1,49 @@
 import "./AllMovies.css";
 import MovieCard from "../MovieCards/MovieCards";
 import PropTypes from "prop-types";
-import SearchMovies from "../Search/search"
-import { useState, useEffect } from "react"
-
+import SearchMovies from "../Search/search";
+import { useState, useEffect } from "react";
 
 // add filtering/search functionality here in APP component
 // set state for search
 // useEffect?
 
-// - filter over the moviesData 
-    // - split the movie title based on (" ") to get an array of words.
-    // - use find() to search through the array to find the first word from 
-    // the title that starts with a user's search value. 
-    // should not be case sensitive: toLowerCase()?
+// - filter over the moviesData
+// - split the movie title based on (" ") to get an array of words.
+// - use find() to search through the array to find the first word from
+// the title that starts with a user's search value.
+// should not be case sensitive: toLowerCase()?
 // - set the state to the result of this filtering
 
-
 function AllMovies({ moviesData }) {
-  const [filteredMovies, setFilteredMovies] = useState([]) 
-  
+  const [filteredMovies, setFilteredMovies] = useState([]);
+
   useEffect(() => {
-    setFilteredMovies([...moviesData])
-}, [moviesData])
+    setFilteredMovies([...moviesData]);
+  }, [moviesData]);
 
-  const filterMovies = (searchTerms) => {   
-    console.log("search terms", searchTerms)
-    if(searchTerms !== undefined) {
-      const searchResults = moviesData.filter((movie) => {
-        const searchTermString = searchTerms.toLowerCase().split(" ").join("")
-        return movie.title.toLowerCase().split(" ").join("").includes(searchTermString)
-      })
-      console.log("SEARCH RESULTS", searchResults)
-    if (searchResults.length === 0 ) {
-      setFilteredMovies([...moviesData])
-
+  const filterMovies = (searchTerms) => {
+    console.log("search terms", searchTerms);
+    // if (searchTerms !== undefined) {
+    const searchResults = moviesData.filter((movie) => {
+      const searchTermString = searchTerms.toLowerCase().split(" ").join("");
+      return movie.title
+        .toLowerCase()
+        .split(" ")
+        .join("")
+        .includes(searchTermString);
+    });
+    console.log("SEARCH RESULTS", searchResults);
+    if (searchResults.length === 0 && searchTerms.length > 0) {
+      setFilteredMovies([]);
+      console.log("should be showing the h2");
+    } else if (searchResults.length === 0) {
+      setFilteredMovies([...moviesData]);
     } else {
-      setFilteredMovies(searchResults)
+      setFilteredMovies(searchResults);
+      // }
     }
-    }
-  }
+  };
 
   const movieCards = filteredMovies.map((movie) => {
     return (
@@ -53,11 +57,14 @@ function AllMovies({ moviesData }) {
     );
   });
   return (
-  <div>
-    <SearchMovies filterMovies={filterMovies}/>
-        <div className="movie-container">{movieCards}</div>
-  </div>
-    )
+    <div>
+      <SearchMovies filterMovies={filterMovies} />
+      {filteredMovies.length === 0 && (
+        <h2>Looks like there's no movies with that title</h2>
+      )}
+      <div className="movie-container">{movieCards}</div>
+    </div>
+  );
 }
 
 AllMovies.propTypes = {
--- src/Components/Search/search.js
+++ src/Components/Search/search.js
@@ -1,26 +1,25 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-function SearchMovies({filterMovies}) {
+function SearchMovies({ filterMovies }) {
+  const [search, setSearch] = useState("");
+  // set state
+  // useEffect to run a filter function (which gets passed down as a prop App)
 
-  const [search, setSearch] = useState()
-// set state 
-// useEffect to run a filter function (which gets passed down as a prop App)
-
-useEffect(( )=> {
-  filterMovies(search);
-}, [search])
+  useEffect(() => {
+    filterMovies(search);
+  }, [search]);
 
   return (
     <form>
       <input
-      type="text"
-      placeholder="Search for a movie"
-      name="search-titles"
-      value={search}
-      onChange={(event) => setSearch(event.target.value)}
+        type="text"
+        placeholder="Search for a movie"
+        name="search-titles"
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
       />
     </form>
-  )
+  );
 }
 
-export default SearchMovies
+export default SearchMovies;
